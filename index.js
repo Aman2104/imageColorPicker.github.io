f6b718628aac26546208b2ba1350f7978cd38840
Aman2104 <uietcseamankumar2125@kuk.ac.in>
2022-10-11 07:32:51 +0530

    mobile-friendly

--- index.js
+++ index.js
@@ -12,23 +12,24 @@ image_input.addEventListener("change", function () {
   reader.readAsDataURL(this.files[0]);
 });
 
-document.querySelector("#display_image").addEventListener("click", () => {
+document.querySelector("#display_image").addEventListener("click", open_EyeDropper);
+document.querySelector(".btn").addEventListener("click", open_EyeDropper);
+
+
+function open_EyeDropper() {
   if (document.querySelector("#display_image").classList.contains("display")) {
     const eyedropper = new EyeDropper();
 
     eyedropper.open().then((res) => {
-      console.log(res);
       document.querySelector(".result").innerHTML = `Hex : ${res.sRGBHex}`;
       document.querySelector(".color-fill").style.background = `${res.sRGBHex}`;
-      document.querySelectorAll(
-        ".color-fill"
-      )[1].style.background = `${res.sRGBHex}`;
+      document.querySelectorAll(".color-fill")[1].style.background = `${res.sRGBHex}`;
       let ans = res.sRGBHex;
       let Rgb = ans.substring(1);
       hexToRgbNew(Rgb);
     });
   }
-});
+}
 var copytext = document.querySelector(".result");
 let copybtn = document.querySelectorAll(".fa-clone");
 
@@ -48,7 +49,6 @@ for (let i = 0; i < copybtn.length; i++) {
 }
 
 function hexToRgbNew(hex) {
-  console.log(hex);
   var arrBuff = new ArrayBuffer(4);
   var vw = new DataView(arrBuff);
   vw.setUint32(0, parseInt(hex, 16), false);
@@ -58,6 +58,6 @@ function hexToRgbNew(hex) {
     ".getRgb"
   ).innerHTML = `rgb(${arrByte[1]}, ${arrByte[2]}, ${arrByte[3]})`;
 
-  console.log(arrByte[1], arrByte[2], arrByte[3]);
+
   return arrByte[1] + "," + arrByte[2] + "," + arrByte[3];
 }
